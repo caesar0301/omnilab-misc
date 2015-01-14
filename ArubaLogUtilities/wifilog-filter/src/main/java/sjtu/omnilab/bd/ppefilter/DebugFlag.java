1b44069abe039ec9565500ad196618669293b89f
Jamin X. Chen <chenxm35@gmail.com>
2015-01-14 23:21:15 +0800

    Move ArubaLogUtilities to ArubaSyslog project of OMNILab

--- ArubaLogUtilities/wifilog-filter/src/main/java/sjtu/omnilab/bd/ppefilter/DebugFlag.java
+++
@@ -1,8 +0,0 @@
-package sjtu.omnilab.bd.ppefilter;
-
-/**
- * Created by chenxm on 11/3/14.
- */
-public class DebugFlag {
-    static boolean debug = true;
-}
--- ArubaLogUtilities/wifilog-filter/src/main/java/sjtu/omnilab/bd/ppefilter/README.md
+++
@@ -1,2 +0,0 @@
-Ping-pong Effect Detection
-=========================
