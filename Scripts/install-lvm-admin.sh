3edc88fd4aa78f2f75195fcf4a21000be0b944c3
Jamin X. Chen <chenxm35@gmail.com>
2015-01-23 12:49:41 +0800

    Add lvm admin installer

---
+++ Scripts/install-lvm-admin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# By Xiaming Chen
+set -e
+sudo -s
+
+EXEC="create-lvm"
+
+curl -L https://github.com/caesar0301/omnilab-misc/raw/master/Scripts/auto-create-lvm.sh \
+     >> /usr/local/bin/$EXEC
+
+chmod +x /usr/local/bin/$EXEC
