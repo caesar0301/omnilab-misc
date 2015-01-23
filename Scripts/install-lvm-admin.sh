10148358865e41228671de1cd8547c9c3e020563
Xiaming <chenxm35@gmail.com>
2015-01-23 21:46:37 +0800

    Update install-lvm-admin.sh

--- Scripts/install-lvm-admin.sh
+++ Scripts/install-lvm-admin.sh
@@ -6,6 +6,6 @@ sudo -s
 EXEC="create-lvm"
 
 curl -L https://github.com/caesar0301/omnilab-misc/raw/master/Scripts/auto-create-lvm.sh \
-     >> /usr/local/bin/$EXEC
+     > /usr/local/bin/$EXEC
 
 chmod +x /usr/local/bin/$EXEC
