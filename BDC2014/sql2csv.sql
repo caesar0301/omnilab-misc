a770d750d14c6f02b94b1e10714bbf8ab470c0fb
Xiaming Chen <chenxm35@gmail.com>
2015-03-20 14:43:22 +0800

    Remove BDC files; We have a independent project for this contest.

--- BDC2014/sql2csv.sql
+++
@@ -1,4 +0,0 @@
-update t_lable_group_comp
-set format_content = REPLACE(REPLACE(format_content, '\r', ''), '\n', ' ');
-
-select * from t_lable_group_comp INTO OUTFILE "t_lable_group_comp" FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '\\' LINES TERMINATED BY '\r\n';
