3d980d014e891c4d8c107dc9e39e07b4764dddde
Xiaming <chen@xiaming.me>
2015-07-24 23:17:57 +0800

    Update config_r.sh

--- Scripts/config_r.sh
+++ Scripts/config_r.sh
@@ -7,10 +7,12 @@ if [ $? == 1 ]; then
   sudo yum install -y R
 fi
 
-R -e "install.packages(c('dplyr', 'tidyr', 'ggplot2', 'data.table'), repo='http://cran.rstudio.com')"
+sudo yum install -y libcurl-devel openssl-devel libxml2-devel libxslt-devel
+
+R -e "install.packages(c('devtools', 'dplyr', 'tidyr', 'ggplot2', 'data.table'), repo='http://cran.rstudio.com')"
 
 R -e "devtools::install_github('jalvesaq/colorout')"
 
-curl https://github.com/caesar0301/omnilab-misc/raw/master/Scripts/.Rprofile -o ~/.Rprofile
+curl https://raw.githubusercontent.com/caesar0301/omnilab-misc/master/Scripts/.Rprofile -o ~/.Rprofile
 
 echo "Done!"
