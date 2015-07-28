76a6e28e5e371fa0ff25c23fc0972279b3434d9a
Xiaming <chen@xiaming.me>
2015-07-28 15:08:10 +0800

    Update config_r.sh

--- Scripts/config_r.sh
+++ Scripts/config_r.sh
@@ -9,7 +9,7 @@ fi
 
 sudo yum install -y libcurl-devel openssl-devel libxml2-devel libxslt-devel
 
-R -e "install.packages(c('devtools', 'dplyr', 'tidyr', 'ggplot2', 'data.table'), repo='http://cran.rstudio.com')"
+R -e "install.packages(c('devtools', 'dplyr', 'tidyr', 'data.table', 'reshape', 'reshape2', 'plyr', 'ggplot2', 'gplots', 'gridExtra', 'ggvis', 'plotrix', 'scatterplot3d', 'colorRamps', 'grDevices', 'RColorBrewer', 'colorspace', 'dichromat', 'MASS', 'Hmisc', 'classInt', 'parallel', 'doBy', 'squash', 'depmixS4', 'entropy', 'psych', 'fields', 'rgdal', 'raster', 'spdep', 'spacetime', 'gstat', 'RandomFields', 'CompRandFld', 'zoo', 'xst', 'geoR', 'maptools', 'mapproj', 'maps', 'osmar', 'OpenStreetMap', 'GISTools', 'splancs', 'PBSmapping', 'deldir', 'ade4', 'ape', 'ncf', 'pgirmess', 'spatial', 'mpmcorrelogram', 'fitdistrplus', 'mixtools', 'mixdist'), repo='http://cran.rstudio.com')"
 
 R -e "devtools::install_github('jalvesaq/colorout')"
 
