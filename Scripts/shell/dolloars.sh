bc6a3d8ec65e9d417944acba28a43c5a83c8170c
Xiaming <chen@xiaming.me>
2015-08-17 17:19:05 +0800

    Update dolloars.sh

--- Scripts/shell/dolloars.sh
+++ Scripts/shell/dolloars.sh
@@ -1,7 +1,7 @@
 # POSIX standard $ commands
-"$@" Stores all the arguments that were entered on the command line, individually quoted ("$1" "$2" ...).
-$#   Stores the number of command-line arguments that were passed to the shell program.
 $0   Stores the first word of the entered command (the name of the shell program).
+$@ Stores all the arguments that were entered on the command line, individually quoted ("$1" "$2" ...).
+$#   Stores the number of command-line arguments that were passed to the shell program.
 $*   Stores all the arguments that were entered on the command line ($1 $2 ...).
 $?   Expands to the decimal exit status of the most recent pipeline.
 $$   Expands to the decimal process ID of the invoked shell.
