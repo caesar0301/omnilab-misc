64f4790d72e39a070bb49dfd46195529fd4c14ad
Xiaming <chen@xiaming.me>
2015-08-17 11:02:31 +0800

    Update ssh_forwarding.sh

--- Scripts/shell/ssh_forwarding.sh
+++ Scripts/shell/ssh_forwarding.sh
@@ -1,9 +1,9 @@
 ## SSH forwarding configurations
 
-# local port forwarding
-ssh -N -f -L 8080:localhost:80 <host>
+# local port 8080 forwarding to remote 80
+ssh -N -f -L 8080:localhost:80 guest@joes-pc
 
-# remote port forwarding
+# remote port 5900 forwarding to local 5900
 ssh -N -f -R 5900:localhost:5900 guest@joes-pc
 
 # multihop port forwarding
